65191eaab47e58e3ade1f5c9289b4427780a6628
sharondiep7 <sdiep7@hawaii.edu>
2020-03-24 13:21:13 -1000

    Update ex4.js

--- Lab 11/ex4.js
+++ Lab 11/ex4.js
@@ -1,16 +1,18 @@
-attributes = "Dan;19; 19.5;-18.5";
-var pieces = attributes.split(",");
+attributes = "Sharon;19;19.5;-18.5";
+var pieces = attributes.split(";");
 for(i=0; i<pieces.length; i++) {
-    console.log(pieces[i], typeof pieces[i]);
+    console.log(pieces[i], typeof pieces[i], isNonNegInt(pieces[i],true));
 }
 
-console.log(pieces.join("-2.2"));
+pieces.forEach(printIt);
+pieces.forEach(
+    function() {console.log( (typeof item== 'string' && item.length > 0)?true:false )}
+);
 
-
-function isNonNegInt(q) {
+function isNonNegInt(q,returnErrors = false) {
     errors = []; // assume no errors at first
     if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
     if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-    return (errors.lenght == 0);
+    return returnErrors ? errors : (errors.length == 0);
 }
