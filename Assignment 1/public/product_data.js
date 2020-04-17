b28bab6bc44c8c0611b02ac22a1c225e0c674663
sharondiep7 <sdiep7@hawaii.edu>
2020-04-16 22:00:28 -1000

    Create product_data.js

---
+++ Assignment 1/public/product_data.js
@@ -0,0 +1,26 @@
+var products = 
+[
+    {
+        "model": "Rose",
+        "price": 5.00,
+    },
+    {
+        "model": "Tiger Lilly",
+        "price": 7.00,
+    },
+    {
+        "model": "Daisy",
+        "price": 4.00,
+    },
+    {
+        "model": "Orchid",
+        "price": 15.00,
+    },
+    {
+        "model": "Gardeina",
+        "price": 10.00,
+    }
+]
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
