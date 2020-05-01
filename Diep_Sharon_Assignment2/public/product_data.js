6b86146af72029bd63f4fe643736fdd09462081d
sharondiep7 <sdiep7@hawaii.edu>
2020-04-30 21:38:18 -1000

    assignment 2

---
+++ Diep_Sharon_Assignment2/public/product_data.js
@@ -0,0 +1,31 @@
+var products = 
+[
+    {
+        "model": "Rose",
+        "price": 5.00,
+        "image": ""
+    },
+    {
+        "model": "Tiger Lilly",
+        "price": 7.00,
+        "image": ""
+    },
+    {
+        "model": "Daisy",
+        "price": 4.00,
+        "image": ""
+    },
+    {
+        "model": "Orchid",
+        "price": 15.00,
+        "image": ""
+    },
+    {
+        "model": "Gardeina",
+        "price": 10.00,
+        "image": ""
+    }
+]
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
