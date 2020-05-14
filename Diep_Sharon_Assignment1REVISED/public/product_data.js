46b9462aadb4b6fc03c52aa4807f9cfa2db496ae
sharondiep7 <sdiep7@hawaii.edu>
2020-05-13 21:38:10 -1000

    ass 1 updates

---
+++ Diep_Sharon_Assignment1REVISED/public/product_data.js
@@ -0,0 +1,81 @@
+var products = 
+[
+    {
+        "model": "'Legend' - Bob Marley and the Wailers",
+        "price": 30.00,
+        "image": "Legend.png"
+    },
+    {
+        "model": "'Please Please Me' - The Beatles",
+        "price": 21.00,
+        "image": "PleasePleaseMe.png"
+    },
+    {
+        "model": "'Hotel California' - The Eagles",
+        "price": 20.00,
+        "image": "HotelCalifornia.png"
+    },
+    {
+        "model": "'Ramones' - Ramones",
+        "price": 30.00,
+        "image": "Ramones.png"
+    },
+    {
+        "model": "'Pet Sounds' - The Beach Boys",
+        "price": 22.00,
+        "image": "PetSounds.png"
+    },
+    {
+        "model": "'Innervisions' - Stevie Wonders",
+        "price": 26.00,
+        "image": "Innervisions.png"
+    },
+    {
+        "model": "'Let It Bleed' - The Rolling Stones",
+        "price": 24.00,
+        "image": "LetItBleed.png"
+    },
+    {
+        "model": "'Revolver' - The Beatles",
+        "price": 19.00,
+        "image": "Revolver.png"
+    },
+    {
+        "model": "'Highway 61 Revisited' - Bob Dylan",
+        "price": 23.00,
+        "image": "Highway61Revisited.png"
+    },
+    {
+        "model": "'Ziggy Stardust' - David Bowie",
+        "price": 21.00,
+        "image": "ZiggyStardust.png"
+    },
+    {
+        "model": "'The Sun Sessions' - Elvis Presley",
+        "price": 35.00,
+        "image": "TheSunSessions.png"
+    },
+    {
+        "model": "'Thriller' - Michael Jackson",
+        "price": 23.00,
+        "image": "Thriller.png"
+    },
+    {
+        "model": "'Graceland' - Paul Simon<",
+        "price": 25.00,
+        "image": "Graceland.png"
+    },
+    {
+        "model": "'Dark Side of the Moon' - Pink Floyd",
+        "price": 28.00,
+        "image": "DarkSideOfTheMoon.jpg"
+    },
+    {
+        "model": " My Time - JK",
+        "price": 37.00,
+        "image": "MyTime.png"
+    }
+]
+if(typeof module != 'undefined') {
+    module.exports.products = products;
+  }
---
+++ Lab 15 #2/ex3.js
@@ -0,0 +1,140 @@
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var myParser = require("body-parser");
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+var session = require('express-session');
+app.use(session({
+    secret: "ITM352 rocks!",   
+    resave: false,
+    saveUninitialized: true
+}));
+
+var user_info_file = './user_data.json';
+var quantity_str;
+
+if (fs.existsSync(user_info_file)) {
+    var file_stats = fs.statSync(user_info_file);
+
+    var data = fs.readFileSync(user_info_file, 'utf-8');
+    var userdata = JSON.parse(data);
+
+    console.log(`${user_info_file} has ${file_stats.size} characters`);
+} else {
+    console.log("hey! " + user_info_file + " doesn't exist!");
+}
+
+app.use(myParser.urlencoded({ extended: true }));
+
+// add a route to get a cookie that may have been set here
+app.get('/set_cookie', function (request, response) {
+    console.log('In GET /set_cookie');
+    var my_name = 'Daniel';
+    response.cookie('your_name', my_name, {maxAge: 5*1000}).send('cookie set'); //Sets name = express
+});
+
+app.get('/use_cookie', function (request, response) {
+    console.log('In GET /use_cookie' , request.cookies);
+   var the_name = request.cookies["your_name"];
+    response.send('Welcome to the Use Cookie page ' + the_name); //Sets name = express
+});
+
+app.get('/use_session', function (request, response) {
+    console.log('In GET /use_session' , request.session);
+    var the_sess_id = request.session.id;
+    response.send('welcome, your session ID is ' + the_sess_id); //Sets name = express
+});
+
+
+app.get("/login", function (request, response) {
+    console.log(request.query); // print out q-str
+    quantity_str = request.query;
+    // Give a simple login form
+    str = `
+<body>
+<h1>Hello ${session.username}! You last logged in on ${session.last_login_time}</h1>
+<h1>${request.query["error"]}</h1>
+<form action="/check_login" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" value=${request.query["username"]} ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+<a href="./register">New user regsiter</a>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/check_login", function (request, response) {
+    // Process login form POST and redirect to logged in page if ok, back to login page if not
+    console.log(request.query);
+    var err_str = "";
+    var login_username = request.body["username"];
+    // check if username exists in reg data. If so, check if password matches
+    if (typeof userdata[login_username] != 'undefined') {
+        var user_info = userdata[login_username];
+        // check if password stored for username matches what user typed in
+        if (user_info["password"] != request.body["password"]) {
+            err_str = `bad_password`;
+        } else {
+            session.username = login_username;
+            var theDate = Date.now();
+            session.last_login_time = theDate;
+            response.end(`${login_username} is logged in with data ${JSON.stringify(quantity_str)} on ${theDate}`);
+            return;
+        }
+
+    } else {
+        err_str = `bad_username`;
+    }
+    response.redirect(`./login?username=${login_username}&error=${err_str}`);
+});
+
+app.get("/register", function (request, response) {
+    // Give a simple register form
+    str = `
+<body>
+<form action="/register_user" method="POST">
+<input type="text" name="username" size="40" placeholder="enter username" ><br />
+<input type="password" name="password" size="40" placeholder="enter password"><br />
+<input type="password" name="repeat_password" size="40" placeholder="enter password again"><br />
+<input type="email" name="email" size="40" placeholder="enter email"><br />
+<input type="submit" value="Submit" id="submit">
+</form>
+</body>
+    `;
+    response.send(str);
+});
+
+app.post("/register_user", function (request, response) {
+    // process a simple register form
+    //console.log(request.body);
+    username = request.body.username;
+    errs = [];
+    // check if username is taken
+
+    if (typeof userdata[username] != 'undefined') {
+        errs.push("username taken");
+    } else {
+        userdata[username] = {};
+    }
+    // is passwrd same as repeat passwrd
+    if (request["body"]["password"] != request["body"]["repeat_password"]) {
+        errs.push("passwords don't match");
+    } else {
+        userdata[username].password = request["body"]["password"];
+    }
+    userdata[username].email = request.body.email;
+
+    console.log(errs.length);
+    if (errs.length == 0) {
+        fs.writeFileSync(user_info_file, JSON.stringify(userdata));
+        response.end(`New user ${username} registered`);
+    } else {
+        response.end(JSON.stringify(errs));
+    }
+});
+
+app.listen(8080, () => console.log(`listening on port 8080`));
